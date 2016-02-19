43336e611500b8e6269f254e15a5712cf008c660
HamiltonChris <chris.ross.hamilton@gmail.com>
2016-02-18 16:50:42 -0800

    Implements pad data function
    Adds function that adds a column of ones to a matrix of input data

--- src/loaddata.py
+++ src/loaddata.py
@@ -1,13 +1,14 @@
+# loaddata.py
+# loads data from mnist data set and puts it into a vectorized form
 import pandas as pd
 import numpy as np
 
 def load_train_data():
-    mnist = pandas.read_csv("../data/train.csv")
+    mnist = pd.read_csv("../data/train.csv")
     labels = one_hot_encoding(mnist["label"].values)
     train_data = mnist.drop("label",1).values
     train_data = train_data * (1/255)
-
-    return train_data, labels
+    return train_data.T, labels
 
 def one_hot_encoding(labels):
     labels = np.array(labels, dtype=np.uint)
@@ -25,4 +26,10 @@ def load_test_data():
     test_data = pandas.read_csv("../data/test.csv")
     test_data = test_data * (1/255)
 
-    return test_data
+    return test_data.T
+
+#pad the data along the column 
+def pad_data(data):
+    length, width = data.shape
+    pad = np.ones((length,1))
+    return np.concatenate((pad,data),axis=1)
